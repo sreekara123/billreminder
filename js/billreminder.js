f18e9d7011fb95b21ca3bc31c93dc9f39f6c0ab5
Sreekara <syachama@purdue.edu>
2018-12-05 02:28:19 -0500

    working prototype

--- js/billreminder.js
+++ js/billreminder.js
@@ -1,5 +1,5 @@
 function signUp() {
-  firebase.auth().createUserWithEmailAndPassword("sreekara123@gmail.com", "123456").catch(function(error) {
+  firebase.auth().createUserWithEmailAndPassword(document.getElementById("email").value, document.getElementById("password").value).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -7,33 +7,88 @@ function signUp() {
   });
 }
 
-function signIn() {
-  firebase.auth().signInWithEmailAndPassword("sreekara123@gmail.com", "123456").catch(function(error) {
-  // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage);
-  });
-}
+
 
 function addEvent() {
   var event = {
-    date: (new Date).toISOString(),
-    eventName: "Hello World",
-    description: "Testing"
+    name: document.getElementById("billName").value,
+    description: document.getElementById("billDes").value,
+    amount: document.getElementById("amount").value,
+    date: document.getElementById("enterdate").value
   };
   var ref = firebase.database().ref('users/' + firebase.auth().currentUser.uid);
   ref.child("events").push(event);
-  retrieveEvents();
+  //retrieveEvents();
+}
+
+var count = 0;
+
+var arr = [];
+function retrieveEvents2() {
+  console.log("im here");
+  firebase.database().ref('users/' + firebase.auth().currentUser.uid).child("events").once("value", function(result) {
+    result.forEach(function(child) {
+      arr.push(child.val());
+    });
+  });
+  retrieveEvents(() => console.log('huzzah, I\'m done!'));
 }
 
 function retrieveEvents() {
-  firebase.database().ref('users/' + firebase.auth().currentUser.uid).child("events").once("value", function(result) {
-    result.forEach(function(child) {
-      console.log(child.val().date);
-    });
-  });
+  count++;
+  console.log("im here too");
+  let len = 0;
+  // arr.forEach(function(element) {
+  //   console.log(len);
+  //   len = len+1;
+  // });
+  let n = arr.length; 
+  console.log(len);
+  if(count%2 === 0) {
+  for (var i = 0; i < n-1; i++) {
+      for (var j = 0; j < n-i-1; j++) {
+          if (arr[j].date > arr[j+1].date) 
+          { 
+              // swap temp and arr[i] 
+              var temp = arr[j]; 
+              arr[j] = arr[j+1]; 
+              arr[j+1] = temp;
+          } 
+        }
+      }
+    }
+
+      if(count % 2 === 0) {
+        document.getElementsByClassName("main")[0].innerHTML = "";
+        for(var i = 0; i < arr.length; i++) {
+
+         //document.getElementsByClassName("main")[0].innerHTML += arr[i].date;
+         //document.getElementsByClassName("main")[0].innerHTML += '<br>';
+         if(new Date(arr[i].date) > new Date()) 
+         document.getElementsByClassName("main")[0].innerHTML += '<div class="w3-container"><h2>' + arr[i].name + '    ' + arr[i].date + '</h2><p>' + arr[i].description + '</p><hr></div><br>';
+        }
+      }
+      //console.log(arr);
+      arr.length = 0;
+
 }
+
+// function sortEntries() {
+//   console.log(n);
+//   for (var i = 0; i < n-1; i++) {
+//     console.log(i);
+//             for (var j = 0; j < n-i-1; j++) {
+//                 if (arr[j].date > arr[j+1].date) 
+//                 { 
+//                     // swap temp and arr[i] 
+//                     console.log(arr[j].date);
+//                     var temp = arr[j]; 
+//                     arr[j] = arr[j+1]; 
+//                     arr[j+1] = temp; 
+//                 } 
+//             }
+//   }
+// }
 
 function addName() {
 console.log(firebase.auth().currentUser.uid);
@@ -47,9 +102,66 @@ firebase.database().ref('users/' + firebase.auth().currentUser.uid).set({
 function signOut() {
   firebase.auth().signOut().then(function() {
     console.log("successful signout");
+    window.location.replace("mainpage.html")
     // Sign-out successful.
   }).catch(function(error) {
     console.log("error in signOut");
     // An error happened.
   });
+}
+
+function billForm() {
+  console.log(document.getElementsByClassName("main")[0]);
+  document.getElementsByClassName("main")[0].innerHTML = '<div id="display"><h2>Enter a new bill</h2><br><input class="w3-input" type="text" placeholder="Enter Bill Name" id="billName"><br><input class="w3-input" type="text" placeholder="Enter Bill Description" id="billDes"><br><input class="w3-input" type="text" placeholder="Enter Amount" id="amount"><br><input class="w3-input" type="date" placeholder="Enter Date" id="enterdate"><br><button class="button" onclick="addEvent()">Submit</button></div>';
+  //document.getElementsByClassName("main")[0].innerHTML = '<div class=\"w3-container w3-card w3-white w3-round w3-margin\"><br><img src=\"img/avatar2.png\" alt=\"Avatar\" class=\"w3-left w3-circle w3-margin-right\" style=\"width:60px\"><span class=\"w3-right w3-opacity\"></span><h4>" + arr[i].name + " " + arr[i].date + "</h4><br><hr class=\"w3-clear\"><p>Location: " + " " + "<br>Date: " + " " + "<br>ZipCode: " + " " + "<br> Description: " + arr[i].description + "<br> Date: " + " " + "</p><div class=\"w3-row-padding\" style=\"margin:0 -16px\"><div class=\"w3-half\"></div><div class=\"w3-half\"></div></div>';
+}
+var arr2 = [];
+function dueTodayStart() {
+  firebase.database().ref('users/' + firebase.auth().currentUser.uid).child("events").once("value", function(result) {
+    result.forEach(function(child) {
+      arr2.push(child.val());
+    });
+  });
+  console.log(arr2);
+  dueToday(() => console.log('huzzah, I\'m done!'));
+}
+var count2 = 0;
+function dueToday() {
+  count2++;
+  console.log("im here too");
+  let len = 0;
+  // arr.forEach(function(element) {
+  //   console.log(len);
+  //   len = len+1;
+  // });
+  let n = arr2.length; 
+  if(count%2 === 0) {
+  for (var i = 0; i < n-1; i++) {
+      for (var j = 0; j < n-i-1; j++) {
+          if (arr2[j].date > arr2[j+1].date) 
+          { 
+              // swap temp and arr[i] 
+              var temp = arr[j]; 
+              arr2[j] = arr2[j+1]; 
+              arr2[j+1] = temp;
+          } 
+        }
+      }
+    }
+
+      if(count2 % 2 === 0) {
+        document.getElementsByClassName("main")[0].innerHTML = "";
+        for(var i = 0; i < arr2.length; i++) {
+
+         //document.getElementsByClassName("main")[0].innerHTML += arr[i].date;
+         //document.getElementsByClassName("main")[0].innerHTML += '<br>';
+         let d1 = new Date(arr2[i].date);
+         let d2 = new Date();
+         if(d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate()) { 
+          document.getElementsByClassName("main")[0].innerHTML += '<div class="w3-container"><h2>' + arr2[i].name + '    ' + arr2[i].date + '</h2><p>' + arr2[i].description + '</p><hr></div><br>';
+         }
+        }
+      }
+      //console.log(arr);
+      arr2.length = 0;
 }
